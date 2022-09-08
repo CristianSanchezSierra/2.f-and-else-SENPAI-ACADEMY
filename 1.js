eb5d723308712a1b7ff49fba6f5ccd759cd7b5f3
Cristian <cristiansr2508@gmail.com>
2022-09-08 19:23:43 -0300

    v1

---
+++ 1.js
@@ -0,0 +1,89 @@
+// PARTE 1
+// let num = prompt("Introduce un numero");
+// if(num %2 == 0){
+//     alert("Es par")
+// }else{
+//     alert("Es impar")
+// }
+
+// PARTE 2
+// let num = prompt("Pon un numero");
+// if(num %5 !== 0){
+//     console.log("No es divisible")
+// }else{
+//     console.log("Es divisible")
+// }
+
+// PARTE 3
+// let num = window.prompt("Eacirbe un numero");
+// if(num %11==0 && num %5==0){
+//     console.log("Divisible")
+// }else{
+//     console.log("No divisible")
+// }
+
+// PARTE 4
+// let num1 = prompt("Escribe un numero");
+// let num2 = prompt("Escribe otro numero");
+// if(num1 > num2){
+//     alert("El mayor es "+num1+" ")
+// }else{
+//     alert("El mayor es "+num2+" ")
+// }
+
+// PARTE 5
+// let num1 = prompt("Mete un numero");
+// let num2 = prompt("Mete otro numero");
+// let comparador = (num1 > num2) ? alert("Es mayor "+num1+" ") : alert("Es mayor "+num2+" ");
+
+// PARTE 6
+// let usu = prompt("Escribe una palabra");
+// let prim = usu.charAt(0)
+// if(isNaN(prim)){
+//     alert("Es String")
+// }else{
+//     alert("Es Integer")
+// }
+
+// PARTE 7
+// Sin saber como resolver (De momento)
+
+// PARTE 8
+// let usu = prompt("Escribe una palabra");
+// if(usu.charAt(0) == usu.charAt(0).toLowerCase()){
+//     alert("Minuscula")
+// }else{
+//     alert("Mayuscula")
+// }
+
+// PARTE 9
+// let bis = prompt("Coloca un año");
+// if(bis % 4 === 0){
+//     alert("Es bisiesto")
+// }else{
+//     alert("No es bisiesto")
+// }
+
+// PARTE 10
+// let aleatorio = Math.floor(Math.random()*11); console.log(aleatorio)
+// let intento = parseInt(prompt("Coloca un numero a ver si adivinas"));
+
+// if(aleatorio === intento){
+//     alert("Buen trabajo");
+// }else{
+//     alert("No corresponde");
+// }
+
+// PARTE 11
+// let edad = prompt("Pon tu edad");
+// if(edad < 13){
+//     console.log("Es niño");
+// } else if (edad < 18){
+//     console.log("Es adolecente");
+// } else {
+//     console.log("Es adolecente");
+// }
+// El error radicaba en la sintaxis del condicional del segundo if; cuando queremos colocar varias condiciones se debe agregar "Else if"
+
+// PARTE 12
+// No comprendido el enunciado
